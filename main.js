6f4701ea93ef779527a77efe8a25b6bac89a4230
laurab-io <io.laurab@gmail.com>
2016-01-14 18:27:37 -0700

    Portfolio Page
    Yay!

---
+++ main.js
@@ -0,0 +1,25 @@
+/*//YouTube API background for 2nd panel
+('#background-video').YTPPlayer({
+  fitToBackground: true,
+  videoId: 'nx8EZ4n9Y' //this needs an ID
+  
+  
+  
+})*/
+
+/*/Neatsy cool dynamic bubble thanks at the bottom     */
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 30];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "Thank You!";
+letterColors =[black, blue];
+if (15> 5) {bubbleShape ="circle";}
+
+else {bubbleShape ="square"}
+drawName(myName, letterColors);
+bounceBubbles()
+
+
